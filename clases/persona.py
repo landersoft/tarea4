2bffa3b39a7bcb0eec3ff98cbe852055cb212747
landersoft <landersoft@gmail.com>
2026-04-26 12:49:04 -0400

    Agregar la superclase persona

---
+++ clases/persona.py
@@ -0,0 +1,15 @@
+# Superclase de docente, y estudiante
+class Persona:
+    def __init__(self, rut, nombre, email):
+        self._rut = rut
+        self._nombre = nombre
+        self._email = email
+
+    def get_nombre(self):
+        return self._nombre
+
+    def get_email(self):
+        return self._email
+
+    def mostrar_info(self):
+        return "Nombre: {} - Email: {}".format(self.get_nombre(), self.get_email())
