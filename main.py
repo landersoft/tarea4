8671323aa73ddee2a0d5e42e0696c15d6e3c0751
landersoft <landersoft@gmail.com>
2026-04-26 13:11:29 -0400

    crear objeto docente

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from clases.estudiante import Estudiante
+from clases.docente import Docente
 import datetime
 
 if __name__=="__main__":
     estudiante1 = Estudiante(rut="123", nombre="Rodrigo Lander", email="landersoft@gmail.com", matricula="20260101",fecha_nacimiento=datetime.datetime.strptime("2000-01-05","%Y-%m-%d"),beca=10)
     print(estudiante1.mostrar_info())
+    docente1 = Docente(rut="1234", nombre="Dr.Alvaro Pérez", email="perez@mail.com", especialidad="Matemáticas")
+    print(docente1.mostrar_info())
 
 
 
